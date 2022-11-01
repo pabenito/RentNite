cae267128ab66335cb123378ce5d134bc0c086bc
pabenito <pedrobenitowork@gmail.com>
2022-11-01 10:11:11 +0100

    app as modules & poi API

--- src/main.py
+++ src/main.py
@@ -1,26 +1,23 @@
-from osmapi import OsmApi
-from fastapi import Depends, FastAPI
+# Immport libraries
+from fastapi import FastAPI
 
+# Import modules
+from opendata import osm
+
+# Create app
 app = FastAPI()
 
-@app.get("/osm/node/{id}")
-async def get_node(id:int, osm=Depends(OsmApi)):
-    return osm.NodeGet(id)
+# Include modules
+app.include_router(
+    osm.router,
+    prefix="/osm",
+    tags=["opendata", "osm"]
+)
 
-@app.get("/osm/map")
-async def get_map(lon:float, lat:float, range:float=1, osm=Depends(OsmApi)):
-    min_longitude, min_latitude, max_longitude, max_latitude = coordinates_to_map_range(lon, lat, range)
-    return osm.Map(min_longitude, min_latitude, max_longitude, max_latitude)
+@app.get("/")
+async def root():
+    return {"message": "Welcome to Rentnite"}
 
-def kilometers_to_degrees(kilometers:float):
-    return kilometers/111 # One longitude or latitude degree diference is 111 km distance
-
-def coordinates_to_map_range(longitude:float, latitude:float, kilometers:float):
-    min_longitude = longitude - kilometers_to_degrees(kilometers)
-    min_latitude = latitude - kilometers_to_degrees(kilometers)
-    max_longitude = longitude + kilometers_to_degrees(kilometers)
-    max_latitude = latitude + kilometers_to_degrees(kilometers)
-    return min_longitude, min_latitude, max_longitude, max_latitude
 
 
 

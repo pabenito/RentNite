fcaa135d0168c761bc84aaa9b938be7a5f1634ce
pabenito <pedrobenitowork@gmail.com>
2022-11-01 18:50:11 +0100

    aemet API added to main app

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 
 # Import modules
-from opendata import osm
+from opendata import osm, aemet
 
 # Create app
 app = FastAPI()
@@ -12,6 +12,11 @@ app.include_router(
     osm.router,
     prefix="/osm",
     tags=["opendata", "osm"]
+)
+app.include_router(
+    aemet.router,
+    prefix="/aemet",
+    tags=["opendata", "aemet"]
 )
 
 @app.get("/")

87ec813284a6f0207b3da559d22e811b812b139e
asiernoide254 <0619930829@uma.es>
2022-11-02 21:19:03 +0100

    Actualizado bookings.py

--- src/entities/bookings.py
+++ src/entities/bookings.py
@@ -1,6 +1,10 @@
 # Import libraries
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
 from database import db as db
+from bson.objectid import ObjectId
+import re
 
 # Create router
 router = APIRouter()
@@ -8,26 +12,62 @@ router = APIRouter()
 # Initialize DB
 bookings = db["bookings"]
 
+# Save possible states for later
+states = ["Accepted", "Declined", "Requested", "Cancelled"]
+
 # API
-@router.get("/")
-async def root():
-    return bookings.find
+@router.post("/")
+async def create(response: Response, state: str, from_: float, to: float, cost: float):
+    if state in states and from_ > 0 and to > 0 and cost > 0:
+        bookings.insert_one({"state": state, "from_": from_, "to": to, "cost": cost})
+        response.status_code = 201
+    else:
+        response.status_code = 400
 
-@router.get("/search")
-async def search_id(id : str | None = None): 
-    if id == None :
-        return bookings.find
-    else :
-        return bookings.find({"_id": id})
+@router.put("/{id}")
+async def update(response: Response, id: str, state: str | None = None, from_: float | None = None, to: float | None = None, cost: float | None = None):
+    data = {"state": state, "from_": from_, "to": to, "cost": cost}
+    data = {k: v for k, v in data.items() if v is not None}
+    
+    for v in data.values():
+        if type(v) is float and (v <= 0):
+            response.status_code = 400
+            return
 
-@router.get("/search")
-async def search_state(state : str | None = None):
-    if state == None :
-        return bookings.find
-    else :
-        return bookings.find({"state": state})
+    if len(data) > 0:
+        try:
+            booking = bookings.find_one_and_update({"_id": ObjectId(id)}, {"$set": data})
+        except:
+            booking = None
 
-@router.post("/create")
-async def create(from_ : int | None, state : str | None, to : int | None) :
-    if from_ != None and state != None and to != None :
-        bookings.insert_one({"from_": from_, "state": state, "to": to})
+    if booking is None:
+        response.status_code = 404
+
+@router.get("/{id}")
+async def get_by_id(response: Response, id: str):
+    try:
+        booking = bookings.find_one({"_id": ObjectId(id)}, {"_id": 0})
+    except:
+        booking = None
+
+    if booking is None:
+        response.status_code = 404
+    else:
+        return booking
+
+@router.get("/range/")
+async def get_range(response: Response, size: int, offset: int = 0):
+    if offset >= 0 and size > 0:
+        return [b for b in bookings.find(projection = {"_id": 0}, skip = offset, limit = size)]
+    else:
+        response.status_code = 400
+
+@router.delete("/{id}")
+async def delete(response: Response, id: str):
+    try:
+        booking = bookings.find_one_and_delete({"_id" : ObjectId(id)})
+    except:
+        booking = None
+    
+    if booking is None:
+        response.status_code = 404

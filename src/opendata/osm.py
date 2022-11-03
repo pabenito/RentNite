a3a697f22dff884a19226568e386bacc2f7b39fd
Gabriel Castillo <castillogabriel@uma.es>
2022-11-03 02:17:28 +0100

    Update osm.py

--- src/opendata/osm.py
+++ src/opendata/osm.py
@@ -1,4 +1,4 @@
-# Immport libraries
+# Import libraries
 from osmapi import OsmApi
 from fastapi import Depends, APIRouter
 
@@ -25,7 +25,6 @@ async def get_map_poi(map: dict = Depends(get_poi)):
 @router.get("/maps/all")
 async def get_map_all(map: dict = Depends(get_map)):
     return map
-
 
 # Auxiliary functions
 

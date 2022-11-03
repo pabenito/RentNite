ffa64d78d05ceb50462e3870af7103b312919d46
Raul <rauldofe7@gmail.com>
2022-11-03 02:16:27 +0100

    bus osm

--- src/opendata/osm.py
+++ src/opendata/osm.py
@@ -9,8 +9,11 @@ router = APIRouter()
 def get_map(latitude: float, longitude: float, range: float=1, osm: OsmApi = Depends(OsmApi)):
     return map_list_to_dict(osm.Map(*coordinates_to_map_range(latitude, longitude, range)))
 
-def get_poi(map: dict = Depends(get_map)):
-    return get_nodes_from_map(map, only_with_tags=True)
+def get_poi(search:str,map: dict = Depends(get_map)):
+    if search == "bus":
+        return get_bus_nodes_from_map(map, only_with_tags=True)
+    else:
+        return get_nodes_from_map(map, only_with_tags=True)
 
 # API
 
@@ -19,13 +22,16 @@ async def get_node(id:int, osm=Depends(OsmApi)):
     return osm.NodeGet(id)
 
 @router.get("/maps/poi")
-async def get_map_poi(map: dict = Depends(get_poi)):
+async def get_map_poi(bus : str= Depends(get_poi),map: dict = Depends(get_poi)):
     return map
 
 @router.get("/maps/all")
 async def get_map_all(map: dict = Depends(get_map)):
     return map
 
+@router.get("/maps/poi/bus")
+async def get_map_poi(map: dict = Depends(get_poi)):
+    return map
 
 # Auxiliary functions
 
@@ -47,6 +53,13 @@ def get_nodes_from_map(map: dict, only_with_tags: bool = False):
             map.pop(id)
     return map
 
+def get_bus_nodes_from_map(map: dict, only_with_tags: bool = False):
+    for id in list(map.keys()):
+        if map.get(id).get("type")!="node":
+            map.pop(id)
+        elif only_with_tags and len(map.get(id).get("tag")) < 2 : # Any tag
+            map.pop(id)
+    return map
 
 def have_common_members(a: set, b: set):
     return len(a.intersection(b)) > 0

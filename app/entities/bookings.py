9af397b0baddc5d6ff7d18ca11eb037f69b264c9
asiernoide254 <0619930829@uma.es>
2022-11-09 21:04:21 +0100

    Añadidos detalles a excepciones y añadida dirección de las casas en bookings.py

--- app/entities/bookings.py
+++ app/entities/bookings.py
@@ -1,6 +1,6 @@
 # Import libraries
-from fastapi import APIRouter, Response
-from app.database import db as db
+from fastapi import APIRouter, HTTPException
+from database import db as db
 from bson.objectid import ObjectId
 from datetime import datetime, date, time
 import re, pydantic
@@ -10,6 +10,7 @@ router = APIRouter()
 
 # Initialize DB
 bookings = db["bookings"]
+houses = db["houses"]
 
 # Save possible states for later
 states = ["Accepted", "Declined", "Requested", "Cancelled"]
@@ -22,65 +23,82 @@ pydantic.json.ENCODERS_BY_TYPE[ObjectId]=str
 async def get():
     return list(bookings.find())
 
+
+
 @router.post("/")
-async def create(response: Response, from_: date, to: date, cost: float, userName: str, houseId: str):
+async def create(from_: date, to: date, cost: float, userName: str, houseId: str):
+    try:
+        house = houses.find_one({"_id": ObjectId(houseId)})
+    except:
+        house = None
+    
+    if house is None:
+        raise HTTPException(
+            status_code=400, detail="No se ha encontrado ninguna casa con la ID proporcionada.")
+
+    # Comprobar si la id introducida corresponde a una casa
+
     from_ = datetime.combine(from_, time.min)
     to = datetime.combine(to, time.min)
 
     if cost > 0 and from_ < to:
         bookings.insert_one({"state": "Requested", "from_":  from_,
-                            "to": to, "cost": cost, "userName": userName, "houseId": houseId})
-        response.status_code = 201
+                            "to": to, "cost": cost, "userName": userName, "houseId": houseId, "houseAddress": house["address"]})
     else:
-        response.status_code = 400
+        raise HTTPException(
+            status_code=400, detail="Coste o fecha incorrectos.")
 
 
 @router.put("/{id}")
-async def update(response: Response, id: str, state: str | None = None, from_: date | None = None, to: date | None = None, cost: float | None = None):
+async def update(id: str, state: str | None = None, from_: date | None = None, to: date | None = None, cost: float | None = None):
+    
+    #Formatear las fechas correctamente si se han proporcionado
     if from_ is not None:
         from_ = datetime.combine(from_, time.min)
     if to is not None:
         to = datetime.combine(to, time.min)
 
+    #Inicializar diccionario con todos los datos a introducir, borrando los que son None
     data = {"state": state, "from_": from_, "to": to, "cost": cost}
     data = {k: v for k, v in data.items() if v is not None}
 
+    #Comprobar si se han introducido 0 datos y lanzar una excepcion si se da el caso
     if len(data) == 0:
-        response.status_code = 400
-        return
+        raise HTTPException(
+            status_code=400, detail="No hay nada que actualizar.")
 
-    if (state is not None and state not in states) and (cost is not None and cost <= 0):
-        response.status_code = 400
-        return
+    #Comprobar si el estado y el coste tienen un formato correcto y lanzar una excepcion en caso contrario
+    if (state is not None and state not in states) or (cost is not None and cost <= 0):
+        raise HTTPException(status_code=400, detail="Valores incorrectos.")
 
     try:
         booking = bookings.find_one({"_id": ObjectId(id)})
-        
-        dfrom_ = from_ or booking["from_"]
-        dto = to or booking["to"]
-        
-        if not dfrom_ < dto:
-            response.status_code = 400
-            return
-
-        booking = bookings.find_one_and_update(
-            {"_id": ObjectId(id)}, {"$set": data})
     except:
         booking = None
 
     if booking is None:
-        response.status_code = 404
+        raise HTTPException(status_code=404, detail="Reserva no encontrada.")
 
+    #Usamos dos variables auxiliares para comprobar si "from_" es anterior a "to" independientemente de si se ha introducido en el PUT o no
+    dfrom_ = from_ or booking["from_"]
+    dto = to or booking["to"]
+
+    if not dfrom_ < dto:
+        raise HTTPException(
+            status_code=400, detail="La fecha de inicio no es anterior a la de fin.")
+
+    booking = bookings.update_one(
+        {"_id": ObjectId(id)}, {"$set": data})
 
 @router.get("/{id}")
-async def get_by_id(response: Response, id: str):
+async def get_by_id(id: str):
     try:
         booking = bookings.find_one({"_id": ObjectId(id)})
     except:
         booking = None
 
     if booking is None:
-        response.status_code = 404
+        raise HTTPException(status_code=404, detail="Reserva no encontrada.")
     else:
         return booking
 
@@ -98,27 +116,29 @@ async def get_by_house_id(houseId: str):
 
 
 @router.get("/state/{state}")
-async def get_by_state(response: Response, state: str):
+async def get_by_state(state: str):
     if state not in states:
-        response.status_code = 400
+        raise HTTPException(
+            status_code=400, detail="El estado introducido no existe.")
 
     return [b for b in bookings.find({"state": state}, {"_id": 0})]
 
 
 @router.get("/range/")
-async def get_range(response: Response, size: int, offset: int = 0):
+async def get_range(size: int, offset: int = 0):
     if offset >= 0 and size > 0:
         return [b for b in bookings.find(projection={"_id": 0}, skip=offset, limit=size)]
     else:
-        response.status_code = 400
+        raise HTTPException(
+            status_code=400, detail="El tamaño o el offset no tienen un formato correcto.")
 
 
 @router.delete("/{id}")
-async def delete(response: Response, id: str):
+async def delete(id: str):
     try:
         booking = bookings.find_one_and_delete({"_id": ObjectId(id)})
     except:
         booking = None
 
     if booking is None:
-        response.status_code = 404
+        raise HTTPException(status_code=404, detail="Reserva no encontrada.")

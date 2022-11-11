9331932c5ebbee119313e5040b15efa09e5f267f
asiernoide254 <0619930829@uma.es>
2022-11-11 20:44:14 +0100

    Get global terminado

--- app/entities/bookings.py
+++ app/entities/bookings.py
@@ -1,9 +1,9 @@
 # Import libraries
-from fastapi import APIRouter, HTTPException
-from database import db as db
+from fastapi import APIRouter, HTTPException, status
+from app.database import db as db
 from bson.objectid import ObjectId
 from datetime import datetime, date, time
-import re, pydantic
+import re
 
 # Create router
 router = APIRouter()
@@ -15,17 +15,13 @@ houses = db["houses"]
 # Save possible states for later
 states = ["Accepted", "Declined", "Requested", "Cancelled"]
 
-# declare Objectid as str
-pydantic.json.ENCODERS_BY_TYPE[ObjectId]=str
-
 # API
 @router.get("/")
 async def get():
     return list(bookings.find())
 
 
-
-@router.post("/")
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create(from_: date, to: date, cost: float, userName: str, houseId: str):
     try:
         house = houses.find_one({"_id": ObjectId(houseId)})
@@ -90,6 +86,7 @@ async def update(id: str, state: str | None = None, from_: date | None = None, t
     booking = bookings.update_one(
         {"_id": ObjectId(id)}, {"$set": data})
 
+
 @router.get("/{id}")
 async def get_by_id(id: str):
     try:
@@ -102,35 +99,34 @@ async def get_by_id(id: str):
     else:
         return booking
 
-
-# @router.get("/userName/{userName}")
-# async def get_by_user_name(userName: str):
-#     userName = re.compile(".*" + userName + ".*",
-#                           re.IGNORECASE)  # type: ignore
-#     return [b for b in bookings.find({"userName": {"$regex": userName}}, {"_id": 0})]
-
-
-# @router.get("/house/{houseId}")
-# async def get_by_house_id(houseId: str):
-#     return [b for b in bookings.find({"houseId": houseId}, {"_id": 0})]
-
-
-# @router.get("/state/{state}")
-# async def get_by_state(state: str):
-#     if state not in states:
-#         raise HTTPException(
-#             status_code=400, detail="El estado introducido no existe.")
-
-#     return [b for b in bookings.find({"state": state}, {"_id": 0})]
-
 # Get global
 @router.get("/search/")
-async def search(userName: str | None, houseId: str | None, state: str | None):
-    #TODO
+async def search(userName: str | None = None, houseId: str | None = None, state: str | None = None):
+    if userName is None and houseId is None and state is None:
+        raise HTTPException(
+            status_code=400, detail="No se han proporcionado parametros de busqueda.")
+
+    params = dict()
+
+    if userName is not None:
+        userName = re.compile(".*" + userName + ".*",
+                              re.IGNORECASE)  # type: ignore
+        params['userName'] = {"$regex": userName}
+
+    if houseId is not None:
+        params['houseId'] = houseId
+
+    if state is not None:
+        if state not in states:
+            raise HTTPException(
+                status_code=400, detail="El estado proporcionado no es valido.")
+        params['state'] = state
+
+    return [b for b in bookings.find(filter=params, projection={"_id": 0})]
 
 
-@router.get("/range/")
-async def get_range(size: int, offset: int = 0):
+@ router.get("/range/")
+async def get_range(size: int, offset: int=0):
     if offset >= 0 and size > 0:
         return [b for b in bookings.find(projection={"_id": 0}, skip=offset, limit=size)]
     else:
@@ -138,7 +134,7 @@ async def get_range(size: int, offset: int = 0):
             status_code=400, detail="El tamaño o el offset no tienen un formato correcto.")
 
 
-@router.delete("/{id}")
+@ router.delete("/{id}")
 async def delete(id: str):
     try:
         booking = bookings.find_one_and_delete({"_id": ObjectId(id)})

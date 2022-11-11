13bbc324580470337162d2b267208508ee2193c5
pabenito <pedrobenitowork@gmail.com>
2022-11-11 13:03:42 +0100

    models.py refactored

--- app/entities/models.py
+++ app/entities/models.py
@@ -1,19 +1,26 @@
-from pydantic import Field, EmailStr, BaseModel as PydanticBaseModel
+from pydantic import Field, EmailStr, BaseModel
 from bson.objectid import ObjectId
 from datetime import datetime
 from enum import Enum
 import uuid
 
-# Define our BaseModel setting config
+# Enums
 
-class BaseModel(PydanticBaseModel):
+class State(Enum):
+    ACCEPTED = "Accepted"
+    DECLINED = "Declined"
+    REQUESTED = "Requested"
+    CANCELLED = "Cancelled"
+
+# Define our ConfigModel for setting config
+
+class ConfigModel(BaseModel):
     class Config:
         arbitrary_types_allowed = True # In order to allow ObjectId
 
-# Entities model
+# Base models
 
-class Message(BaseModel):
-    id: ObjectId = Field(alias="_id", default_factory=uuid.uuid4)
+class MessageBase(BaseModel):
     sender_id: str
     sender_username: str
     date: datetime
@@ -22,8 +29,7 @@ class Message(BaseModel):
     house_id: str | None = None
     chat_id: str | None = None
 
-class Chat(BaseModel):
-    id: ObjectId = Field(alias="_id", default_factory=uuid.uuid4)
+class ChatBase(BaseModel):
     house_address: str 
     booking_from: datetime
     booking_to: datetime
@@ -33,13 +39,11 @@ class Chat(BaseModel):
     guest_id : str 
     guest_username : str
 
-class User(BaseModel):
-    id: ObjectId = Field(alias="_id", default_factory=uuid.uuid4)
+class UserBase(BaseModel):
     username: str
     email: EmailStr  
 
-class House(BaseModel):
-    id: ObjectId = Field(alias="_id", default_factory=uuid.uuid4)
+class HouseBase(BaseModel):
     address: str 
     capacity: int 
     price: int 
@@ -47,14 +51,7 @@ class House(BaseModel):
     bathrooms: int 
     owner_name: str = Field(alias="ownerName")
 
-class State(Enum):
-    ACCEPTED = "Accepted"
-    DECLINED = "Declined"
-    REQUESTED = "Requested"
-    CANCELLED = "Cancelled"
-
-class Booking(BaseModel):
-    id: ObjectId = Field(default_factory=uuid.uuid4, alias="_id")
+class BookingBase(BaseModel):
     state: State
     from_: datetime
     to: datetime 
@@ -63,9 +60,43 @@ class Booking(BaseModel):
     house_id: str = Field(alias="houseId")
     house_address: str = Field(alias="houseAddress")
 
+# Entities models
+
+class Message(MessageBase, ConfigModel):
+    id: ObjectId = Field(alias="_id", default_factory=uuid.uuid4)
+
+class Chat(ChatBase, ConfigModel):
+    id: ObjectId = Field(alias="_id", default_factory=uuid.uuid4)
+
+class User(UserBase, ConfigModel):
+    id: ObjectId = Field(alias="_id", default_factory=uuid.uuid4)
+
+class House(HouseBase, ConfigModel):
+    id: ObjectId = Field(alias="_id", default_factory=uuid.uuid4)
+
+class Booking(BookingBase, ConfigModel):
+    id: ObjectId = Field(default_factory=uuid.uuid4, alias="_id")
+
+# Response models
+
+class MessageResponse(MessageBase):
+    id: str
+
+class ChatResponse(ChatBase):
+    id: str
+
+class UserResponse(UserBase):
+    id: str
+
+class HouseResponse(HouseBase):
+    id: str
+
+class BookingResponse(BookingBase):
+    id: str
+
 # Constructors models (allows None in all field in order to add them one by one)
 
-class ChatConstructor(PydanticBaseModel):
+class ChatConstructor(BaseModel):
     house_address: str | None
     booking_from: datetime | None
     booking_to: datetime | None
@@ -75,7 +106,7 @@ class ChatConstructor(PydanticBaseModel):
     guest_id : str | None 
     guest_username : str | None
 
-class MessageConstructor(PydanticBaseModel):
+class MessageConstructor(BaseModel):
     sender_id: str | None
     sender_username: str | None
     date: datetime | None

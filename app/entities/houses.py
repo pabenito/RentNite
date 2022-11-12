6269d7cec6923bdcfa81ad0a8cc63ac9719483e1
Carlos-PB <0619967480@uma.es>
2022-11-12 19:31:38 +0100

    Added ownerId, image and location to the houses

--- app/entities/houses.py
+++ app/entities/houses.py
@@ -10,12 +10,18 @@ router = APIRouter()
 # Initialize DB
 houses = db["houses"]
 bookings = db["bookings"]
+users = db["users"]
 
 # API
 @router.get("/")
 async def get(address: str | None = None, capacity: int | None = None, price: float | None = None, rooms: int | None = None, 
-              bathrooms: int | None = None, ownerName: str | None = None, offset: int = 0, size: int = 10):
-    filter = {"capacity": capacity, "price": price, "rooms": rooms, "bathrooms": bathrooms}
+              bathrooms: int | None = None, ownerId: str | None = None, ownerName: str | None = None, image: str | None = None, 
+              latitude: float | None = None, longitude: float | None = None, offset: int = 0, size: int = 10):
+    if offset < 0 or size <= 0:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, "Invalid offset or size.")
+            
+    filter = {"capacity": capacity, "price": price, "rooms": rooms, "bathrooms": bathrooms, 
+              "ownerId": ownerId, "image": image, "latitude": latitude, "longitude": longitude}
     filter = {k: v for k, v in filter.items() if v is not None}
 
     if address is not None:
@@ -23,9 +29,6 @@ async def get(address: str | None = None, capacity: int | None = None, price: fl
 
     if ownerName is not None:
         filter["ownerName"] = {"$regex": re.compile(".*" + ownerName + ".*", re.IGNORECASE)}
-
-    if offset < 0 or size <= 0:
-        raise HTTPException(status.HTTP_400_BAD_REQUEST, "Invalid offset or size.")
 
     return list(houses.find(filter, {"_id": 0}, skip = offset, limit = size))
 
@@ -42,24 +45,34 @@ async def get_by_id(id: str):
     return house
 
 @router.post("/", status_code = status.HTTP_201_CREATED)
-async def create(address: str, capacity: int, price: float, rooms: int, bathrooms: int, ownerName: str):
-    if capacity <= 0 or price < 0 or rooms <= 0 or bathrooms <= 0 or not re.fullmatch("[a-zA-Z]+( [a-zA-Z]+)*", ownerName):
+async def create(address: str, capacity: int, price: float, rooms: int, bathrooms: int, 
+                 ownerId: str, image: str, latitude: float, longitude: float):
+    if capacity <= 0 or price < 0 or rooms <= 0 or bathrooms <= 0:
         raise HTTPException(status.HTTP_400_BAD_REQUEST, "Invalid parameters.")
+
+    try:
+        owner = users.find_one({"_id": ObjectId(ownerId)}, {"_id": 0, "username": 1})
+    except:
+        owner = None
+
+    if owner is None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, "No user was found with the given ID.")
     
-    houses.insert_one({"address": address, "capacity": capacity, "price": price, 
-                       "rooms": rooms, "bathrooms": bathrooms, "ownerName": ownerName})
+    houses.insert_one({"address": address, "capacity": capacity, "price": price, "rooms": rooms, "bathrooms": bathrooms, 
+                       "ownerId": ownerId, "ownerName": owner["username"], "image": image, "latitude": latitude, "longitude": longitude})
 
 @router.put("/{id}")
-async def update(id: str, address: str | None = None, capacity: int | None = None, 
-                 price: float | None = None, rooms: int | None = None, bathrooms: int | None = None):
-    parameters = {"address": address, "capacity": capacity, "price": price, "rooms": rooms, "bathrooms": bathrooms}
+async def update(id: str, address: str | None = None, capacity: int | None = None, price: float | None = None, rooms: int | None = None, 
+                 bathrooms: int | None = None, image: str | None = None, latitude: float | None = None, longitude: float | None = None):
+    if ((capacity or 1) <= 0) or ((price or 1) < 0) or ((rooms or 1) <= 0) or ((bathrooms or 1) <= 0):
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, "Invalid parameters.")
+    
+    parameters = {"address": address, "capacity": capacity, "price": price, "rooms": rooms, 
+                  "bathrooms": bathrooms, "image": image, "latitude": latitude, "longitude": longitude}
     parameters = {k: v for k, v in parameters.items() if v is not None}
 
     if len(parameters) == 0:
         raise HTTPException(status.HTTP_400_BAD_REQUEST, "No data was given to update the house.")
-
-    if ((capacity or 1) <= 0) or ((price or 1) < 0) or ((rooms or 1) <= 0) or ((bathrooms or 1) <= 0):
-        raise HTTPException(status.HTTP_400_BAD_REQUEST, "Invalid parameters.")
 
     try:
         house = houses.find_one_and_update({"_id": ObjectId(id)}, {"$set": parameters})
@@ -80,10 +93,14 @@ async def delete(id: str):
         raise HTTPException(status.HTTP_404_NOT_FOUND, "House not found.")
 
 @router.get("/owner/{ownerName}/guests")
-async def get_guests_from_owner(ownerName: str):
+async def get_guests_by_owner_name(ownerName: str):
     ownerName = re.compile(".*" + ownerName + ".*", re.IGNORECASE) # type: ignore
 
-    houses_ids = houses.find({"ownerName": {"$regex": ownerName}}, {"_id": 1})
+    houses_ids = list(houses.find({"ownerName": {"$regex": ownerName}}, {"_id": 1}))
+
+    if len(houses_ids) == 0:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, "Owner name not found.")
+
     houses_ids = [{"houseId": str(house_id.get("_id"))} for house_id in houses_ids]
     
     return list(bookings.distinct("guestName", {"$or": houses_ids}))

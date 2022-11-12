4f2349bff23e6e1901b55e85c8824fb9e34cd218
Raul <rauldofe7@gmail.com>
2022-11-12 02:41:34 +0100

    Users list houses/bookings

--- app/entities/users.py
+++ app/entities/users.py
@@ -10,17 +10,21 @@ router = APIRouter()
 
 # Initialize DB
 users = db["users"]
+houses = db["houses"]
+bookings = db["bookings"]
 
 # API
 @router.get("/")
 async def root():
     return {"message": "Welcome to users microservice"}
 
+#Inserta un usuario en la base de datos
 @router.post("/")
 async def create(response: Response, username: str, email: str):
     users.insert_one({"username": username, "email": email})
     response.status_code = 201
 
+#Actualiza un usuario 
 @router.put("/{id}")
 async def update(response: Response,id: str, username: str | None = None, email: str | None = None):
     data = {"username": username, "email": email}
@@ -30,9 +34,7 @@ async def update(response: Response,id: str, username: str | None = None, email:
         response.status_code = 400
         return
 
-    try:
-        user = users.find_one({"_id": ObjectId(id)})
-        
+    try: 
         user = users.find_one_and_update(
             {"_id": ObjectId(id)}, {"$set": data})
     except:
@@ -41,11 +43,12 @@ async def update(response: Response,id: str, username: str | None = None, email:
     if user is None:
         response.status_code = 404
 
+#Devuelve un usuario por su id
 @router.get("/{id}")
 async def get_by_id(response: Response, id: str):
     try:
         user = users.find_one({"_id": ObjectId(id)}, {"_id": 0})
-    except:
+    except Exception:
         user = None
 
     if user is None:
@@ -53,3 +56,43 @@ async def get_by_id(response: Response, id: str):
     else:
         return user
 
+#Borra un usuario
+@router.delete("/{id}")
+async def get_by_id(response: Response, id: str):
+    try:
+        user = users.find_one_and_delete({"_id": ObjectId(id)})
+    except Exception:
+        user = None
+
+    if user is None:
+        response.status_code = 404
+
+#Devuelve la lista de casas de un usuario
+@router.get("/{user_id}/houses")
+async def get_houses_from_user(response: Response, user_id: str):
+    try:
+        user = users.find_one({"_id": ObjectId(user_id)}, {"_id": 0})
+    except Exception:
+        user = None
+
+    if user is None:
+        response.status_code = 404
+    else:
+        try:
+            username = user.get("username")
+            username = re.compile(".*" + username + ".*", re.IGNORECASE)
+            params = dict()
+            params['ownerName'] = user.get("username")
+            return list(houses.find(filter=params, projection={"_id": 0}))
+        except:
+            response.status_code = 404
+
+#Devuelve la lista de reservas de un usuario
+@router.get("/{user_id}/bookings")
+async def get_bookings_from_user(response: Response, user_id: str):
+    try:
+        params = dict()
+        params['guestId'] = user_id
+        return list(bookings.find(filter=params, projection={"_id": 0}))
+    except:
+        response.status_code = 404     

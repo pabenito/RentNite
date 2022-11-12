cc4be7c33dd3a0009f597f765020099459dfa234
Gabriel Castillo <castillogabriel@uma.es>
2022-11-12 17:25:03 +0100

    Update ratings.py
    mthod delete done

--- app/entities/ratings.py
+++ app/entities/ratings.py
@@ -132,61 +132,30 @@ async def create(
 
     return created_rate.to_response()    
     
-     
-     
-     
-        
-@router.put("/{id}")
-async def update(id: str, date_: date | None = None, rating: int | None = None):
-    if date_ is not None:
-        date_ = datetime.combine(date_, time.min)
-    
-
-    data = {"date": date_, "rating": rating}
-    data = {k: v for k, v in data.items() if v is not None}
-
-    if len(data) == 0:
-        response.status_code = 400
-        return
-
-    if (rating is not None or (rating < 0) or (rating > 5) ):
-        response.status_code = 400
-        return
-
-    try:
-        rating = ratings.find_one_and_update(
-            {"_id": ObjectId(id)}, {"$set": data})
-    except Exception:
-        rating = None
-
-    if rating is None:
-        response.status_code = 404
         
 @router.get("/{id}")
-async def get_by_id(response: Response, id: str):
+async def get_by_id(id: str):
     try:
-        rating = ratings.find_one({"_id": ObjectId(id)}, {"_id": 0})
+        return Rating.parse_obj(ratings.find_one({"_id": ObjectId(id)})).to_response
     except Exception:
-        rating = None
-
-    if rating is None:
-        response.status_code = 404
-    else:
-        return rating
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail=f"There is no rating with that id: {id}.")
     
-@router.get("/user/{user_id}")
-async def get_by_user_id(user_id: str):
-    user_id = re.compile(".*" + user_id + ".*",
-                          re.IGNORECASE)  # type: ignore
-    return [r for r in ratings.find({"user_id": {"$regex": user_id}}, {"_id": 0})]
 
 
 @router.delete("/{id}")
-async def delete(response: Response, id: str):
+async def delete(id: str):
     try:
-        rating = ratings.find_one_and_delete({"_id": ObjectId(id)})
+        rated : Rating = Rating.parse_obj(ratings.find_one_and_delete({"_id": ObjectId(id)}))
     except Exception:
-        rating = None
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail=f"There is no rating with that id: {id}.")
 
-    if rating is None:
-        response.status_code = 404
+    if not rated:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail=f"There is no rating with that id: {id}.")
+
+    return rated.to_response()

3d6b9205afe3abf932a5fe231be4b92a85f20bd3
Gabriel Castillo <castillogabriel005@gmail.com>
2022-11-27 02:13:20 +0100

    intente hacer cosas

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 
 # Import modules
 from .entities import router as entities
@@ -11,6 +12,24 @@ from .web import router as web
 
 # Create app
 app = FastAPI()
+
+# cors
+origins = [
+    "http://i.imgur.com",
+    "https://i.imgur.com",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:8000/houses/#loaded"
+    "https://127.0.0.1:8000",
+    "https://127.0.0.1:8000/houses/#loaded"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
@@ -33,6 +52,7 @@ app.include_router(
     web.router,
     tags=["web"]
 )
+
 
 @app.get("/")
 async def root():

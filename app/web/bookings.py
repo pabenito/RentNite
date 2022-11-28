af9cd785c2e0956a6faf29b74604cb1bab1cd224
asiernoide254 <0619930829@uma.es>
2022-11-28 13:37:18 +0100

    POST hecho en web/bookings para form

--- app/web/bookings.py
+++ app/web/bookings.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Request, Cookie
+from fastapi import APIRouter, Request, Cookie, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from datetime import date
 from ..entities import bookings as bookings_api
 from ..entities import houses as houses_api
 from ..entities.models import *
@@ -23,3 +24,9 @@ def booking_details(request: Request, id: str):
     user = "Asier Gallego"
     booking = bookings_api.get_by_id(id)
     return templates.TemplateResponse("bookingDetails.html", {"request": request, "booking": booking, "house": houses_api.get_by_id(booking["house_id"]), "user": user, "State": State})
+
+@router.post("/{id}/requestBooking", response_class=HTMLResponse)
+def create_booking(request: Request, id: str, user = Cookie(default=None), state: State = Form(), from_: date = Form(), to: date = Form(), guest_id: str = Form(), cost: float = Form()):
+    # Request a booking given a house id
+    booking: BookingPost = BookingPost(house_id=id, from_=from_, to=to, guest_id=guest_id, cost=cost)
+    bookings_api.create(booking)

02249441bef309338d3c5fc51c047f57a89540ce
Gabriel Castillo <castillogabriel005@gmail.com>
2022-12-01 21:18:36 +0100

    Update profile.py

--- app/web/profile.py
+++ app/web/profile.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Request, Cookie, Form, File, UploadFile
+from fastapi import APIRouter, Request, Cookie, Form, File, UploadFile, HTTPException
 from fastapi.responses import HTMLResponse,RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from ..entities import users as users_api
 from ..entities import ratings as ratings_api
 from app.entities import models
-from app.web import login
+from app.web import login as login_api
 #from flickrapi import FlickrAPI
 
 router = APIRouter()
@@ -19,23 +19,24 @@ api_key="b3b172dc37a0267d33cf70ee2d8303fc"
 
 @router.get("/", response_class=HTMLResponse)
 def perfil_usuario(request: Request):
-    s = None
-    if login.get_current_user():
-        s = login.Singleton()
+    user = __chechUser()
 
-    return templates.TemplateResponse("profile.html", {"request": request, "user": users_api.get_by_id(s.user), "rating": ratings_api.get(None, s.user, None, None, None, None, None), "identificador": s.user})
+
+    return templates.TemplateResponse("profile.html", {"request": request, "user": users_api.get_by_id(user), "rating": ratings_api.get(None, user, None, None, None, None, None), "identificador": user})
 
 
 @router.post("/{id}/addRate", response_class=HTMLResponse)
 def add_Rate(request: Request, id: str, estrellas: int = Form()):
-    s = login.Singleton()
-    ratings_api.create(s.user, id, None, estrellas)
+    user = __chechUser()
+
+    ratings_api.create(user, id, None, estrellas)
 
     return perfil_usuario(request)
 
 @router.post("/{id}/uploadPhoto", response_class=HTMLResponse)
 def upload_photo(request: Request, id: str, file: bytes = File()):
-    s = login.Singleton()
+    user = __chechUser()
+
     #lickr.authenticate_via_browser
     #flickr.upload("perfil",fileobj=file,title="RentNitePrueba",is_public=1)
 
@@ -43,11 +44,21 @@ def upload_photo(request: Request, id: str, file: bytes = File()):
 
 @router.get("/{id}/edit", response_class=HTMLResponse)
 def edit(request: Request, id: str):
-    return templates.TemplateResponse("profile.html", {"request": request, "user": users_api.get_by_id(id), "rating": ratings_api.get(None, id, None, None, None, None, None), "identificador": id,"editable":True})
+    user = __chechUser()
+    return templates.TemplateResponse("profile.html", {"request": request, "user": users_api.get_by_id(user), "rating": ratings_api.get(None, user, None, None, None, None, None), "identificador": user,"editable":True})
 
 @router.post("/save", response_class=HTMLResponse)
 def save(request: Request,id: str=Form(),password:str=Form(),username:str=Form(),email:str=Form()):
-    print(id)
-    users_api.update(id=id,username=username,email=email,password=password)
+    user = __chechUser()
+
+    users_api.update(id=user,username=username,email=email,password=password)
     
     return perfil_usuario(request)
+
+# Private methods
+def __chechUser():
+    session = login_api.Singleton()
+    if session.user is None:
+        raise HTTPException(
+            status_code=401, detail="No se ha iniciado sesión.")
+    return session.user

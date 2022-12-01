64ec3be0fc4464b1cc5cbcfa099b5af021bc51e4
Gabriel Castillo <castillogabriel005@gmail.com>
2022-12-01 22:01:21 +0100

    Update profile.py

--- app/web/profile.py
+++ app/web/profile.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Request, Cookie, Form, File, UploadFile, HTTPException
-from fastapi.responses import HTMLResponse,RedirectResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from ..entities import users as users_api
@@ -12,8 +12,8 @@ router = APIRouter()
 
 templates = Jinja2Templates(directory="templates")
 
-api_secret="d0009bb5d0d8f8f8"
-api_key="b3b172dc37a0267d33cf70ee2d8303fc"
+api_secret = "d0009bb5d0d8f8f8"
+api_key = "b3b172dc37a0267d33cf70ee2d8303fc"
 #flickr = FlickrAPI(api_key=api_key,secret=api_secret)
 
 
@@ -22,10 +22,12 @@ def perfil_usuario(request: Request):
     user = __chechUser()
     return templates.TemplateResponse("profile.html", {"request": request, "user": users_api.get_by_id(user), "rating": ratings_api.get(None, user, None, None, None, None, None), "identificador": user})
 
+
 @router.get("/{id}", response_class=HTMLResponse)
 def perfil_usuario_distinto(request: Request, id: str):
     user = __chechUser()
-    return templates.TemplateResponse("profile.html", {"request": request, "user": users_api.get_by_id(id), "rating": ratings_api.get(None, id, None, None, None, None, None), "identificador": user, "perfil":id})
+    return templates.TemplateResponse("profile.html", {"request": request, "user": users_api.get_by_id(id), "rating": ratings_api.get(None, id, None, None, None, None, None), "identificador": user, "perfil": id})
+
 
 @router.post("/{id}/addRate", response_class=HTMLResponse)
 def add_Rate(request: Request, id: str, estrellas: int = Form()):
@@ -35,29 +37,35 @@ def add_Rate(request: Request, id: str, estrellas: int = Form()):
 
     return perfil_usuario(request)
 
+
 @router.post("/{id}/uploadPhoto", response_class=HTMLResponse)
 def upload_photo(request: Request, id: str, file: bytes = File()):
     user = __chechUser()
 
-    #lickr.authenticate_via_browser
-    #flickr.upload("perfil",fileobj=file,title="RentNitePrueba",is_public=1)
+    # lickr.authenticate_via_browser
+    # flickr.upload("perfil",fileobj=file,title="RentNitePrueba",is_public=1)
 
     return perfil_usuario(request)
+
 
 @router.get("/{id}/edit", response_class=HTMLResponse)
 def edit(request: Request, id: str):
     user = __chechUser()
-    return templates.TemplateResponse("profile.html", {"request": request, "user": users_api.get_by_id(user), "rating": ratings_api.get(None, user, None, None, None, None, None), "identificador": user,"editable":True})
+    return templates.TemplateResponse("profile.html", {"request": request, "user": users_api.get_by_id(user), "rating": ratings_api.get(None, user, None, None, None, None, None), "identificador": user, "editable": True})
+
 
 @router.post("/save", response_class=HTMLResponse)
-def save(request: Request,password:str=Form(),username:str=Form(),email:str=Form()):
+def save(request: Request, password: str = Form(), username: str = Form(), email: str = Form()):
     user = __chechUser()
 
-    users_api.update(id=user,username=username,email=email,password=password)
-    
+    users_api.update(id=user, username=username,
+                     email=email, password=password)
+
     return perfil_usuario(request)
 
 # Private methods
+
+
 def __chechUser():
     session = login_api.Singleton()
     if session.user is None:

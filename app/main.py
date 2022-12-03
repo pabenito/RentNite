1cbb7f21e20c2faa9f883d2dded430e3c217092d
asiernoide254 <0619930829@uma.es>
2022-12-03 11:00:45 +0100

    Usuario redireccionado a houses/ al logearse

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from app.web import login
-from app.web import profile
 from fastapi.responses import RedirectResponse
 
 
@@ -14,8 +13,6 @@ from .entities import router as entities
 from .opendata import router as opendata
 from .web import router as web
 
-base_url = "http://127.0.0.1:8000"
-
 # Create app
 app = FastAPI()
 
@@ -23,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"]
 )
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -40,31 +37,28 @@ app.include_router(
 app.include_router(
     entities.router,
     prefix="/entities",
-    tags=["entities"],
-
+    tags=["entities"]
 )
 
 app.include_router(
     web.router,
-    tags=["web"],
-
+    tags=["web"]
 )
 
 #oauth2
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
 
-@app.get("/", response_class=RedirectResponse)
+@app.get("/")
 async def root():
-    return "/login"
+    return RedirectResponse("/login")
 
 @app.post('/token')
 async def generate_token(request: Request,form_data: OAuth2PasswordRequestForm = Depends()):
     user = await login.authenticate_user(form_data.username, form_data.password)
     if not user:
-       
         return login.login(request,"Usario o contraseña no validos")
-        
+
     singleton = login.Singleton()
-    singleton.user=user
-    return RedirectResponse(base_url + "/profile", status_code=status.HTTP_303_SEE_OTHER )
+    singleton.user = user
+    return RedirectResponse("/houses", status_code=status.HTTP_303_SEE_OTHER)

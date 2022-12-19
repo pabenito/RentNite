2ab283565481a842d89576783b4461d276046b37
pabenito <pedrobenitowork@gmail.com>
2022-12-20 00:29:22 +0100

    tried but not succeded, try with httpx instead of requests

--- app/web/login.py
+++ app/web/login.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, Cookie, Depends, Form, HTTPException, status
+from fastapi import APIRouter, Request, Cookie, Depends, Form, HTTPException, status, Response, Body
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -7,28 +7,14 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from passlib.hash import sha256_crypt
 from ..entities.models import *
 from fastapi_sso.sso.google import GoogleSSO
+import requests
 
 router = APIRouter()
 
-# PATRON SINGLETON
+session = requests.session()
 
-
-class Singleton (object):
-
-    class __Singleton:
-        def __init__(self):
-            self.user = None
-
-        def __user__(self):
-            return self.user
-
-    instance = None
-
-    def __new__(cls):
-        if not Singleton.instance:
-            Singleton.instance = Singleton.__Singleton()
-        return Singleton.instance
-
+base_url="http://127.0.0.1:8000"
+headers = {"accept": "application/json", "Content-Type": "application/json"}
 
 templates = Jinja2Templates(directory="templates")
 
@@ -54,29 +40,14 @@ def login_Google(email: str, username: str):
     user = users_api.general_get(email=email)
     if not user:
         user = users_api.createAUX(username, email)
-    singleton = Singleton()
-    singleton.user = user["id"]
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    try:
-        user = token
-    except:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail='Invalid username or password'
-        )
-    finally:
-        login(Request)
-
-    return await users_api.get_by_id(user)
-
+    print(f"Hey, google callback. {base_url}/login/user user_id:{user['id']}")
+    requests.post(base_url + "/login/user", headers = headers, data=user["id"])
+    print("End")
 
 @router.get("/", response_class=HTMLResponse)
 def login(request: Request, login_error: str = "", registration_error: str = "", registration_success_message: str = ""):
     return templates.TemplateResponse("login.html", {"request": request, "login_error": login_error, "registration_error": registration_error,
                                                      "registration_success_message": registration_success_message})
-
 
 @router.post("/register", response_class=HTMLResponse)
 def create_user(request: Request, username: str = Form(), correo: str = Form(), password: str = Form()):
@@ -101,12 +72,20 @@ async def generate_token(request: Request, form_data: OAuth2PasswordRequestForm 
     user = await authenticate_user(form_data.username, form_data.password)
     if not user:
         return login(request, "Usuario o contraseña no validos")
-    singleton = Singleton()
-    singleton.user = user
+    requests.post(base_url + "/login/user", data={"user_id":user.id})
     return RedirectResponse("/houses", status_code=status.HTTP_303_SEE_OTHER)
 
-def get_user():
-    return Singleton().__user__()
+@router.post('/user')
+def generate_token(response: Response, user_id: str = Body()):
+    print(f"Hey, post {user_id}")
+    response.set_cookie("user_id", user_id)
+    return user_id
 
-def redirect():
-    return RedirectResponse("/login", status_code = status.HTTP_303_SEE_OTHER)
+@router.get('/user')
+def get_user_id(user_id: str = Cookie()):
+    return user_id
+
+def get_user():
+    user_id = requests.get(base_url + "/login/user").json().get("user_id")
+    print(user_id)
+    return user_id

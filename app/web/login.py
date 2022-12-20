660146f46f9624dd0b87319c9a3afdfc984501bf
pabenito <pedrobenitowork@gmail.com>
2022-12-20 22:56:03 +0100

    login

--- app/web/login.py
+++ app/web/login.py
@@ -1,3 +1,4 @@
+from typing import Union
 from fastapi import APIRouter, Request, Cookie, Depends, Form, HTTPException, status, Response, Body
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
@@ -7,20 +8,24 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from passlib.hash import sha256_crypt
 from ..entities.models import *
 from fastapi_sso.sso.google import GoogleSSO
-from requests_futures.sessions import FuturesSession
+
+base_url="http://127.0.0.1:8000"
 
 router = APIRouter()
 
-session: FuturesSession = FuturesSession()
-
-base_url="http://127.0.0.1:8000"
-headers = {"accept": "application/json", "Content-Type": "application/json"}
-
 templates = Jinja2Templates(directory="templates")
+
+user_id: Union[str, None] = "default"
 
 # OAUTH2
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
+@router.get("/", response_class=HTMLResponse)
+def login(request: Request, login_error: str = "", registration_error: str = "", registration_success_message: str = ""):
+    return templates.TemplateResponse("login.html", {"request": request, "login_error": login_error, "registration_error": registration_error,
+                                                     "registration_success_message": registration_success_message})
+
+# Usuario con contraseña
 
 async def authenticate_user(email: str, password: str):
     user = users_api.general_get(email=email)
@@ -36,20 +41,6 @@ def verify_password(user: User, password: str):
     return sha256_crypt.verify(password, user["password_hash"])
 
 
-def login_Google(email: str, username: str):
-    user = users_api.general_get(email=email)
-    if not user:
-        user = users_api.createAUX(username, email)
-    print(f"Hey, google callback. {base_url}/login/user user_id:{user['id']}")
-    result = session.post(base_url + "/login/user", headers = headers, json={"user_id" : user["id"]}).result()
-    print("End")
-    return result
-
-@router.get("/", response_class=HTMLResponse)
-def login(request: Request, login_error: str = "", registration_error: str = "", registration_success_message: str = ""):
-    return templates.TemplateResponse("login.html", {"request": request, "login_error": login_error, "registration_error": registration_error,
-                                                     "registration_success_message": registration_success_message})
-
 @router.post("/register", response_class=HTMLResponse)
 def create_user(request: Request, username: str = Form(), correo: str = Form(), password: str = Form()):
     try:
@@ -60,36 +51,48 @@ def create_user(request: Request, username: str = Form(), correo: str = Form(), 
 
     return login(request, registration_success_message="Usuario registrado con exito")
 
+@router.post('/token')
+async def generate_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_id_ = await authenticate_user(form_data.username, form_data.password)
+    if not user:
+        return login(request, "Usuario o contraseña no validos")
+    response = RedirectResponse(base_url)
+    global user_id
+    user_id = user_id_
+    response.set_cookie("user_id", user_id_)
+    return response
 
-@router.get("/logout", response_class=HTMLResponse)
-def logout(request: Request):
-    salida = Singleton()
-    salida.user = None
-    return RedirectResponse("/")
+# Google
 
-def get_user():
-    user_id = session.get(base_url + "/login/user").result().json()
-    print(user_id)
-    return user_id
+@router.get("/google")
+async def google_login():
+    """Generate login url and redirect"""
+    return await google_sso.get_login_redirect()
+
+@router.get("/google/callback")
+async def google_callback(request: Request):
+    """Process login response from Google and return user info"""
+    user_google = await google_sso.verify_and_process(request)
+
+    user: User = users_api.general_get(email=user_google.email)
+    if not user:
+        user = users_api.createAUX(user_google.username, user_google.email)
+
+    redirect: RedirectResponse = RedirectResponse(base_url)
+    global user_id
+    user_id = user["id"]
+    redirect.set_cookie("user_id", user["id"])
+    return redirect
+
+@router.get("/logout")
+def logout():
+    response = RedirectResponse(base_url)
+    global user_id
+    user_id = "deleted"
+    response.delete_cookie("user_id")
+    return response
 
 # Cookies
 
-@router.post('/token')
-async def generate_token(request: Request, form_data: OAuth2PasswordRequestForm = Depends()):
-    user = await authenticate_user(form_data.username, form_data.password)
-    if not user:
-        return login(request, "Usuario o contraseña no validos")
-    result = session.post(base_url + "/login/user", data={"user_id":user.id}).result()
-    return RedirectResponse("/houses", status_code=status.HTTP_303_SEE_OTHER)
-    return result
-
-@router.post('/user')
-async def post_user_id(response: Response, user_id: Union[str, None] = Body(default=None)):
-    print(f"Hey, post {user_id}")
-    response.set_cookie("user_id", user_id)
-    return user_id
-
-@router.get('/user')
-def get_user_id(user_id: Union[str, None] = Cookie(default=None)):
-    print("get user cookie")
+def get_user():
     return user_id
--- main.py
+++ main.py
@@ -31,24 +31,6 @@ google_sso = GoogleSSO(
     environ["APP_URL"]
 )
 
-
-@app.get("/google/login")
-async def google_login():
-    """Generate login url and redirect"""
-    return await google_sso.get_login_redirect()
-
-
-@app.get("/google/callback")
-async def google_callback(request: Request):
-    """Process login response from Google and return user info"""
-    user = await google_sso.verify_and_process(request)
-    
-    login.login_Google(user.email,user.display_name)
-    return RedirectResponse("/houses", status_code=status.HTTP_303_SEE_OTHER)
-
-
-
-
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
